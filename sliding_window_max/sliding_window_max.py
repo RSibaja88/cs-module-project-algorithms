50d4e8fa3eca89d1878b43a5b320cbabb7f0b246
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-08-13 22:10:18 -0400

    product of all other nums

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,26 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+
+def sliding_window_max(nums, k):
+    max = 0
+    n = len(nums)
+    arr1 = []
+
+    for i in range(n - k + 1):
+        max = nums[i]
+        for j in range(1, k):
+            if nums[i + j] > max:
+                max = nums[i + j]
+        arr1.append(max)
+    return arr1
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
